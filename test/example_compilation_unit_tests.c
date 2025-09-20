683bcc72947578684893b9fad019f56b67b3469a
ccgargantua <ccgargantua@proton.me>
2025-09-20 11:30:31 -0400

    Split cmake to subdirectory for tests

--- test/example_compilation_unit_tests.c
+++ test/example_compilation_unit_tests.c
@@ -1,6 +1,6 @@
-#include <example_compilation_unit.h>
+#include "../src/example_compilation_unit.h"
 
-#include <rktest.h>
+#include "rktest.h"
 
 TEST(example_tests, add_vector2i_test)
 {
@@ -8,5 +8,5 @@ TEST(example_tests, add_vector2i_test)
     Vector2i v2 = {3, 4};
     Vector2i v3 = add_vector2i(v1, v2);
     EXPECT_EQ(v3.x, v1.x + v2.x);
-    EXPECT_EQ(v3.y, v1.y - v2.y);
+    EXPECT_EQ(v3.y, v1.y + v2.y);
 }
